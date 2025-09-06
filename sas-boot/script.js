c18b6d1fcc64fd163046c17c905f922cd943646f
Felipe Vásquez <felipevasquez977@gmail.com>
2025-09-05 20:54:30 -0400

    carrito v2

--- sas-boot/script.js
+++ sas-boot/script.js
@@ -5,13 +5,14 @@ function añadirProducto() {
     // declaración de variables que agarran los elementos del DOM, como precio y carrito
     let precio = document.getElementById("price1");
     let carrito = document.getElementById("carrito");
+    let producto = document.getElementById("producto1");
     let carritoItems = [];
 
     // evento click para agregar el producto al carrito, y mostrar alerta
     // TODO: Falta implementar funcion, no detecta el click
     precio.addEventListener("click", function () {
         carritoItems.push(precio.textContent);
-        alert("Has seleccionado el producto con precio: " + precio.textContent);
+        alert("Has seleccionado el producto " + producto.textContent + " con precio: " + precio.textContent);
     });
 
     // evento click para mostrar los productos en el carrito, o alerta si está vacío
@@ -19,7 +20,7 @@ function añadirProducto() {
         if (carritoItems.length === 0) {
             alert("Tu carrito está vacío.");
         } else {
-            alert("Productos en tu carrito:\n" + carritoItems.join("\n"));
+            alert("Productos en tu carrito:\n" + producto.textContent + "\n" + carritoItems.join("\n"));
         }
     });
 }
